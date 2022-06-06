6f2cbde257cd929b70cb0e2ae62a6e0a2df48041
Shawn Barnes <nwahs.barnes@gmail.com>
2022-06-06 18:10:21 -0400

    Add bugs-on-a-plane

---
+++ level-one/exercises/bugs-on-a-plane/app.js
@@ -0,0 +1,27 @@
+var form = document.getElementById("airline-form");
+var submit = document.getElementById("submit");
+var query = document.querySelector;
+
+function formAlert() {
+    let firstName = form.elements["first-name"].value;
+    let lastName = form.elements["last-name"].value;
+    let age = form.elements["age"].value;
+    let gender = form.elements["gender"].value;
+    let location = form.elements["travel-location"].value;
+    let diet = [];
+    if (form.elements['vegan'].checked) {
+        diet.push(document.getElementById("vegan").value);
+    }
+    if (form.elements['gluten'].checked) {
+        diet.push(document.getElementById('gluten').value);
+    }
+    if (form.elements['paleo'].checked) {
+        diet.push(document.getElementById('paleo').value);
+    }
+
+
+    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
+}
+
+
+submit.addEventListener("click", formAlert);
