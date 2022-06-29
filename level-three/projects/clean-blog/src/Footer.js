decc580ce19e2c9e620173a7ae23ab584b3c9e24
Shawn Barnes <nwahs.barnes@gmail.com>
2022-06-29 14:26:39 -0400

    Fix font-awesome icons

--- level-three/projects/clean-blog/src/Footer.js
+++ level-three/projects/clean-blog/src/Footer.js
@@ -6,9 +6,9 @@ export default function Footer() {
         <footer>
             <div className="social-media-container">
                 <div className="social-media">
-                    <a href="https://www.twitter.com"><i class="fa-brands fa-twitter-square fa-4x"></i></a>
-                    <a href="https://www.facebook.com"><i class="fa-brands fa-facebook-square fa-4x"></i></a>
-                    <a href="https://www.github.com"><i class="fa-brands fa-github-square fa-4x"></i></a>
+                    <a href="https://www.twitter.com"><i className="fa-brands fa-twitter-square fa-4x"></i></a>
+                    <a href="https://www.facebook.com"><i className="fa-brands fa-facebook-square fa-4x"></i></a>
+                    <a href="https://www.github.com"><i className="fa-brands fa-github-square fa-4x"></i></a>
                 </div>
                 <p className="copyright">Copyright © Your Website 2022</p>
             </div>
