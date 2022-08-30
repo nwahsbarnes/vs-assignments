1938ff57190defa09f4375bf0b73355a0840d7b0
Shawn Barnes <nwahs.barnes@gmail.com>
2022-08-30 13:18:13 -0400

    Add Navbar initial component

---
+++ level-four/projects/theme-changer/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Navbar(props) {
+    return (
+        <nav>
+            <div>Home</div>
+            <div>About</div>
+            <div>Contact</div>
+        </nav>
+    );
+}
+
+export default Navbar;
